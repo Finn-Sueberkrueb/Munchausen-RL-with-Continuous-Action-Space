68bbbd0700be7938632a3b321c337c31a21f756c
Marcel Brucker <marcel.brucker@gmx.de>
2021-05-16 14:41:52 +0200

    msac trains too

--- scripts/run_v1.py
+++ scripts/run_v1.py
@@ -9,11 +9,11 @@ env = gym.make("InvertedPendulumPyBulletEnv-v0")
 # env.render()
 
 # train new agent
-model = SAC("MlpPolicy", env, verbose=1, tensorboard_log="./trained_agents/sac_invertedPendulum/tensorboard/")
+# model = MSAC("MlpPolicy", env, verbose=1, tensorboard_log="./trained_agents/msac_invertedPendulum_tensorboard")
 # load last trained agent
-# model = SAC.load("./trained_agents/sac_invertedPendulum", env, verbose=1, tensorboard_log="./trained_agents/sac_invertedPendulum_tensorboard")
-model.learn(total_timesteps=5000, log_interval=4)
-model.save("./trained_agents/sac_invertedPendulum")
+model = MSAC.load("./trained_agents/msac_invertedPendulum", env, verbose=1, tensorboard_log="./trained_agents/msac_invertedPendulum_tensorboard")
+model.learn(total_timesteps=10000, log_interval=4)
+model.save("./trained_agents/msac_invertedPendulum")
 
 print("replay trained agent")
 obs = env.reset()
