22600838a1c465b86720aa17af46bc70471e73f3
Marcel Brucker <marcel.brucker@gmx.de>
2021-05-21 22:17:40 +0200

    corrected path

--- setup/preinstall_vm.sh
+++ setup/preinstall_vm.sh
@@ -3,5 +3,5 @@
 # ./preinstall_vm.sh
 
 VM=mock-instance
-gcloud compute scp --project "tum-adlr-ss21-08" ~/Repositories/tum-adlr-ss21-08/install_vm.sh $VM:~
+gcloud compute scp --project "tum-adlr-ss21-08" ~/Repositories/tum-adlr-ss21-08/setup/install_vm.sh $VM:~
 gcloud compute scp --project "tum-adlr-ss21-08" ~/.ssh/id_rsa.pub ~/.ssh/id_rsa $VM:~/.ssh
