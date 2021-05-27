eaba79f7065954b7b2af29e55f1243eed819d182
Marcel Brucker <marcel.brucker@gmx.de>
2021-05-27 12:49:55 +0200

    new VM with more power

--- setup/connect_vm.sh
+++ setup/connect_vm.sh
@@ -6,7 +6,7 @@
 # gcloud config set compute/region REGION
 # gcloud compute zones list
 # gcloud config set compute/zone ZONE
-VM=mock-instance
-gcloud config set compute/region europe-west3
-gcloud config set compute/zone europe-west3-c
+VM=instance-c2
+gcloud config set compute/region europe-west4
+gcloud config set compute/zone europe-west4-a
 gcloud compute ssh "$VM" --project "tum-adlr-ss21-08"
--- setup/preinstall_vm.sh
+++ setup/preinstall_vm.sh
@@ -2,6 +2,6 @@
 # chmod +x preinstall_vm.sh
 # ./preinstall_vm.sh
 
-VM=mock-instance
+VM=instance-c2
 gcloud compute scp --project "tum-adlr-ss21-08" ~/Repositories/tum-adlr-ss21-08/setup/install_vm.sh $VM:~
 gcloud compute scp --project "tum-adlr-ss21-08" ~/.ssh/id_rsa.pub ~/.ssh/id_rsa $VM:~/.ssh
