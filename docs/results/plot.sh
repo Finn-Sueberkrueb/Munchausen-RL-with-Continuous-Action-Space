88be2b68dfbf7ea0b6333a3843a38cef53b52dc0
Marcel Brucker <marcel.brucker@gmx.de>
2021-06-04 20:12:42 +0200

    merged some files from branch dev/finn

---
+++ docs/results/plot.sh
@@ -0,0 +1,8 @@
+python  docs/results/all_plots.py \
+        -a sac msac \
+        --env Ant HalfCheetah Hopper Walker \
+        --print-n-trials \
+        -max 1000000 \
+        -s 4 \
+        -f  docs/results
+
